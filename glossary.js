a76c81a1ad6dfb35fcec322db1eb2eaa00169c2d
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-05-05 23:18:57 +0200

    v0.1
    Add another button.
    Add responsive.
    Edit card behavior.
    Edit CSS.
    Split JS code in multiples files.

---
+++ glossary.js
@@ -0,0 +1,135 @@
+export const glossary = [
+    {
+        word: "Agile",
+        definition: "Les méthodes de développement en approche agile visent la satisfaction réelle des besoins d’informatisation du client.Elles l’impliquent pendant tout le développement et permettent une grande réactivité à ses demandes.",
+    },
+    {
+        word: "AJAX",
+        definition: "AJAX est l’acronyme d'Asynchronous Javascript and XML. Il permet de construire des applications Web et des sites web dynamiques interactifs sur les postes clients en se servant de différentes technologies: JavaScript, CSS, JSON, XML, DOM.La combinaison de ces technologies permet d'améliorer la maniabilité et le confort d'utilisation des applications internet riches (abréviation RIA : Rich Interface Application).",
+    },
+    {
+        word: "Application web",
+        definition: "Une application web (aussi appelée web app de l’anglais) est une application manipulable grâce à un navigateur web.",
+    },
+    {
+        word: "CMS",
+        definition: "Content Management System : système de gestion de contenu. Un CMS est une solution facilitant la conception et la mise à jour de sites ou applications Web. Un CMS dispose de fonctionnalités fournissant une chaîne de publication pour mettre en ligne de l’information. Il sépare les opérations de gestion de la forme de celles du contenu, structure le contenu et facilite la gestion des utilisateurs et de leurs droits.",
+    },
+    {
+        word: "Composant",
+        definition: "(Source Wikipédia du 17/05/2016) Un composant logiciel est un élément de base d'un système informatique plus complexe dans lequel les éléments sont organisés entre eux, rendent un service prédéfini et sont capables de communiquer entre eux ainsi qu’avec d'autres composants extérieurs. La programmation orientée composant a pris de l'ampleur avec le développement objet.",
+    },
+    {
+        word: "CRUD",
+        definition: "(Source Wikipédia du 21/12/2017) L'acronyme informatique anglais CRUD (pour create, read, update, delete) désigne les quatre opérations de base pour la persistance des données, en particulier le stockage d'informations en base de données. Plus généralement, il désigne les opérations permettant la gestion d'une collection d'éléments.",
+    },
+    {
+        word: "CSRF",
+        definition: "(Source Wikipédia du 07/09/2017) En sécurité informatique, le Cross-Site Request Forgery est un type de vulnérabilité des services d'authentification web. L’objet de cette attaque est de transmettre à un utilisateur authentifié une requête HTTP falsifiée qui pointe sur une action interne au site, afin qu'il l'exécute sans en avoir conscience et en utilisant ses propres droits.",
+    },
+    {
+        word: "DOM",
+        definition: "Document Object Model. Interface de programmation normalisée par le W3C permettant à des scripts d'examiner et de modifier le contenu du navigateur web. Les objets DOM peuvent représenter une fenêtre, un document, une phrase, un style...",
+    },
+    {
+        word: "ECMAScript",
+        definition: "ECMAScript est un ensemble de spécifications mises en œuvre dans différents langages de script. Ces spécifications sont standardisées par l’organisation ECMA International.",
+    },
+    {
+        word: "FAQ",
+        definition: "Frequently Asked Questions. Liste des questions les plus posées par les utilisateurs avec les réponses associées. Évite l’engorgement des sites d’appel.",
+    },
+    {
+        word: "Feuille de style",
+        definition: "(Source Wikipédia du 31/12/2017) Les feuilles de style en cascade, généralement appelées CSS de l'anglais Cascading Style Sheets, forment un langage informatique qui décrit la présentation des documents HTML et XML. Les standards définissant CSS sont publiés par le World Wide Web Consortium (W3C).",
+    },
+    {
+        word: "Framework",
+        definition: "Appelé en français cadre d’applications, c’est un ensemble de classes d’objet, utilisables pour créer des applications informatiques. Le framework fournit au développeur des objets d’interface (bouton, menu, fenêtres, boîtes de dialogue), des objets de service (collections, conteneurs) et des objets de persistance (accès aux fichiers et aux bases de données) prêts à l’emploi. Le développeur peut donc s’appuyer sur ces classes et se concentrer sur les aspects métier de son application.",
+    },
+    {
+        word: "Gabarit de mise en page",
+        definition: "Un gabarit de mise en page est un modèle de mise en page où l'on peut placer des images, du texte, et tout autre contenu multimédia. Il permet de distinguer le fond, c’est à dire l’information, de la forme, c’est-à-dire la présentation. Il agit comme un modèle dans lequel seuls certains éléments sont modifiables.",
+    },
+    {
+        word: "HTTP",
+        definition: "(Source Wikipédia du 19/12/2017) HyperText Transfer Protocol, littéralement « protocole de transfert hypertexte » est un protocole de communication client-serveur. Les clients HTTP les plus connus sont les navigateurs Web permettant à un utilisateur d'accéder à un serveur contenant les données.",
+    },
+    {
+        word: "Interface utilisateur web (ou page web) dynamique ou statique",
+        definition: "(Source Wikipédia 31/01/2017) Une page web dynamique est une page web générée à la demande, par opposition à une page web statique. Le contenu d'une page web dynamique peut donc varier en fonction d'informations (heure, nom de l'utilisateur, formulaire rempli par l'utilisateur, etc.) qui ne sont connues qu'au moment de sa consultation. À l'inverse, le contenu d'une page web statique est, à priori, identique à chaque consultation.",
+    },
+    {
+        word: "JSON",
+        definition: "(Source Wikipédia du 02/01/2018) JavaScript Object Notation. Format de données textuelles dérivé de la notation des objets du langage JavaScript. Il permet de représenter de l’information structurée comme le permet XML par exemple. Un document JSON a pour fonction de représenter de l'information accompagnée d'étiquettes permettant d'en interpréter les divers éléments, sans aucune restriction sur le nombre de celles-ci.",
+    },
+    {
+        word: "Langage de balise",
+        definition: "Les langages à base de balises servent surtout à structurer ou formater des documents. HTML est un exemple de langage de balise, il permet de formater les pages web, par exemple en soulignant certains mots.",
+    },
+    {
+        word: "Langage de script client et langage de script serveur",
+        definition: "Un langage de script client fait en général référence à des programmes dans un contexte web qui s'exécutent sur le navigateur web côté client, par opposition au langage de script serveur s'exécutant sur un serveur Web. Javascript est un exemple de langage de script client. PHP est un exemple de langage de script serveur.",
+    },
+    {
+        word: "Middleware",
+        definition: "En architecture informatique, un middleware est un logiciel tiers qui crée un réseau d'échange d'informations entre différentes applications informatiques.",
+    },
+    {
+        word: "Objet",
+        definition: "Le développement objet est basé sur l’identification, la modélisation, puis la programmation de composants (classes). Considérés comme des boîtes noires, on ne peut utiliser ces composants qu’à travers leur interface publique. Cette interface est constituée de propriétés (caractéristique visible de l’objet), de méthodes (ce que l’on peut demander de faire à un objet) et de messages émis par l’objet (auxquels on peut réagir par l’exécution d’une procédure). Les concepts objet sont présents à tous les niveaux des architectures des applications informatiques.",
+    },
+    {
+        word: "OWASP",
+        definition: "(Source Wikipédia du 25/09/2017) Open Web Application Security Project est une communauté en ligne travaillant sur la sécurité des applications Web. Sa philosophie est d'être à la fois libre et ouverte à tous. Elle a pour vocation de publier des recommandations de sécurisation Web et de proposer aux internautes, administrateurs et entreprises des méthodes et outils de référence permettant de contrôler le niveau de sécurisation de ses applications Web.",
+    },
+    {
+        word: "Référencement naturel",
+        definition: "Le référencement est l'action de référencer, c'est-à-dire mentionner quelque chose ou y faire référence. Sur Internet, le travail de référencement consiste à améliorer le positionnement et la visibilité de sites dans des pages de résultats de moteurs de recherche ou d'annuaires. Le référencement naturel consiste à travailler les éléments internes et externes des sites pour permettre de donner plus facilement les informations à la fois aux internautes mais également aux moteurs de recherche.",
+    },
+    {
+        word: "Service Web",
+        definition: "(Source Wikipédia du 03/04/2017) Un service web est un protocole d'interface informatique de la famille des technologies web permettant la communication et l'échange de données entre applications et systèmes hétérogènes dans des environnements distribués.",
+    },
+    {
+        word: "SGBD",
+        definition: "(Source Wikipédia du 11/11/2017) Un Système de Gestion de Base de Données est un logiciel système destiné à stocker et à partager des informations dans une base de données, en garantissant la qualité, la pérennité et la confidentialité des informations, tout en cachant la complexité des informations.",
+    },
+    {
+        word: "SQL",
+        definition: "Structured Query Language. Langage de requêtes, basé sur l’algèbre relationnelle, utilisé pour manipuler les données dans une base de données relationnelles.",
+    },
+    {
+        word: "Style défensif",
+        definition: "Programmer dans un style défensif consiste à écrire le code de manière à anticiper les risques d'erreur et les comportements inattendus, par exemple en contrôlant que les entrées utilisateurs sont correctes. L'absence de cette anticipation peut mener à des failles de sécurité telles que les débordements de tampon.",
+    },
+    {
+        word: "Transaction SQL",
+        definition: "Une transaction SQL correspond à un mécanisme permettant de s’assurer que l’ensemble des différentes opérations composant la transaction sont toutes menées à leur terme. Si l’une des opérations de la transaction se passe mal, la base de données reviendra à son état antérieur.",
+    },
+    {
+        word: "UML",
+        definition: "Unified Modeling Language. Formalisme basé sur les concepts de développement objet, qui permet de modéliser graphiquement une application informatique à toutes les étapes de son développement.",
+    },
+    {
+        word: "W3C",
+        definition: "World Wide Web Consortium. Le W3C est une organisation internationale dont les membres, des éditeurs de logiciels, des constructeurs, des développeurs et des utilisateurs, s’entendent pour faire la promotion de technologies destinées à tirer le meilleur du Web. Les avis et recommandations du W3C tiennent souvent lieu de normes.",
+    },
+    {
+        word: "WCAG",
+        definition: "Web Content Accessibility Guidelines. Recueil de préconisations pour rendre le Web accessible aux handicapés, aveugles, mal voyants, sourds, déficients cognitifs ou moteurs.",
+    },
+    {
+        word: "Workflow",
+        definition: "(Source Wikipédia du 31/07/2017) De façon pratique, le workflow sert à décrire le circuit des traitements, les tâches à répartir entre les différents acteurs d'un processus, les délais, les contrôles et modes de validation, et à fournir à chacun des acteurs les informations nécessaires à l'exécution de sa tâche.",
+    },
+    {
+        word: "XML",
+        definition: "(Source Wikipédia du 08/12/2017) L'eXtensible Markup Language est un métalangage informatique de balisage générique. Cette syntaxe est dite « extensible » car elle permet de définir différents langages avec chacun leur vocabulaire et leur grammaire.",
+    },
+    {
+        word: "XSS",
+        definition: "(Source Wikipédia du 06/10/2017) Le cross-site scripting est un type de faille de sécurité des sites web permettant d'injecter du contenu dans une page, permettant ainsi de provoquer des actions sur les navigateurs web visitant la page.",
+    },
+
+];
---
+++ main.js
@@ -0,0 +1,61 @@
+import { glossary } from '/glossary.js';
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = `Il vous reste ${glossary.length} mots à apprendre.`;
+
+const card = document.querySelector(".card");
+const front = card.querySelector(".card__front");
+const back = card.querySelector(".card__back");
+
+const buttons = document.querySelectorAll("button");
+const textFront = document.querySelector(".card__front h2");
+const textBack = document.querySelector(".card__back p");
+
+var previousCardIndex = -1;
+
+const getRandomInt = (max) => {
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + 1));
+}
+
+const drawCard = () => {
+
+    let index = getRandomInt(glossary.length - 1);
+    while (index === previousCardIndex) {
+        index = getRandomInt(glossary.length - 1);
+    }
+
+    gsap.set(front, { clearProps: "all" });
+    gsap.set(back, { clearProps: "all" });
+    flipped = false;
+
+    textFront.textContent = glossary[index].word;
+    textBack.textContent = glossary[index].definition;
+    previousCardIndex = index;
+}
+
+buttons.forEach(button => button.addEventListener("click", drawCard));
+
+let flipped = false;
+card.addEventListener("click", () => {
+
+
+
+    if (!flipped) {
+        gsap.to(front, { rotationY: -180, duration: 1 });
+        gsap.to(back, { rotationY: 0, duration: 1 });
+        flipped = true;
+        return;
+    }
+
+    gsap.to(front, { rotationY: 0, duration: 1 });
+    gsap.to(back, { rotationY: 180, duration: 1 });
+    flipped = false;
+
+}
+
+);
+
+
+drawCard();
